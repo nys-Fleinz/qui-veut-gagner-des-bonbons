4aae039fc0f61517e8327db846a98516e735260b
nys-Fleinz <ethan.seulin@gmail.com>
2024-11-27 22:16:20 +0100

    le CSV AAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Joueur.java
@@ -0,0 +1,6 @@
+class Joueur {
+    String nom;
+    int points;
+    int bonneReponse=0;
+    int mauvaiseReponse=0;
+}
--- jeu.java
+++ jeu.java
@@ -1,13 +1,32 @@
+import extensions.CSVFile;
+import extensions.File;
+
+
 class bonbon extends Program{
 
-    Joueurs[] joueurs;
+    CSVFile pokedex = loadCSV("questions.csv");
 
     Joueur newJoueur(String nom) {
         Joueur joueur = new Joueur();
         joueur.nom = nom;
         joueur.points = 0;
+        joueur.bonneReponse = 0;
+        joueur.mauvaiseReponse = 0;
         return joueur;
     }
+
+    Joueurs CreerJoueurs(int nombreJoueurs) {
+        Joueurs tab = new Joueurs();
+        tab.joueur = new Joueur[nombreJoueurs];
+        for(int i=0; i<nombreJoueurs; i=i+1) {
+            println("Insérez le nom du joueur numéro"+i+": ");
+            tab.joueur[i] = newJoueur(readString());
+        }
+        return tab;
+    }
+
+
+
 
 
     void algorithm() {
